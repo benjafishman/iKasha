7d51d7f6a140e8233e9c2b9d5097a898f54a112f
Ben Fishman <benjafishman@gmail.com>
2017-02-05 20:52:46 -0500

    - creating model to keep track of all questions associated with a book-chapter-verse

--- Kasha/models.py
+++ Kasha/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 class Question(models.Model):
 	question_text = models.TextField(max_length=500)
@@ -7,5 +8,9 @@ class Question(models.Model):
 	en_verse_text = models.TextField(null=True, max_length=500, blank=True)
 	book_source = models.CharField(null=True, max_length=500, blank=True)
 
+
+class Source_Questions_Listing(models.Model):
+	book_chapter_sentence = models.CharField(max_length=100)
+	questions = ArrayField(models.CharField(max_length=200), blank=True)
 
 
