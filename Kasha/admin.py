e08b390164175aeb10b356ecdbbe4768ace8e1d1
Ben Fishman <benjafishman@gmail.com>
2017-02-07 23:45:32 -0500

    fixed admin page bug to show verses field

--- Kasha/admin.py
+++ Kasha/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Question, BookChapterQuestions, SourceQuestion
 
 class QuestionAdmin(admin.ModelAdmin):
-	fields = ['pub_date', 'question_text', 'en_verse_text','he_verse_text','book_source','tags']
+	fields = ['pub_date', 'question_text', 'en_verse_text','he_verse_text','book_source','verses']
 	list_display = ('question_text', 'verses','pub_date')
 admin.site.register(Question, QuestionAdmin)
 
